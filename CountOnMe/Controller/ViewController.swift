c164866dc5fbab778bea87081f68456c82b16775
Romain Buewaert <romain.buewaert@gmail.com>
2021-05-27 11:09:21 +0200

    Minor Change

--- CountOnMe/Controller/ViewController.swift
+++ CountOnMe/Controller/ViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    // View Life cycles
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
